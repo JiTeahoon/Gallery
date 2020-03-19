40aa85f7537ff6f521862e4f9bf32d585807ec67
ucal <ucal@mfindstudio.com>
2020-03-19 18:13:53 +0900

    실시간 채팅 프로그래밍 완성

--- socket.js
+++ socket.js
@@ -10,13 +10,20 @@ module.exports = (server, app, sessionMiddleware) => {
 
     io.on('connection', function (socket) {
         socketList.push(socket);
-        console.log('User Join');
+        console.log(`유저 숫자 : ${socketList.length}`);
 
         socketList.forEach(function (item, i) {
-            console.log(socket.request.session.authId);
-            if (item != socket) {
+            console.log(`유저 인덱스 : ${i}, 유저 ID : ${item.request.session.authId}`);
+
+            // socketList.forEach(function (childitem, i) {
+            //     childitem.emit('EXIT', `${item.request.session.authId}`);
+            // });
+            // socketList.forEach(function (childitem, i) {
+            //     childitem.emit('ENTER', `${item.request.session.authId}`);
+            // });
+
+            if (item != socket)
                 item.emit('SEND', `${socket.request.session.authId}님이 들어오셧습니다.`);
-            }
         });
 
         socket.on('SEND', function (msg) {
@@ -27,13 +34,22 @@ module.exports = (server, app, sessionMiddleware) => {
             });
         });
 
+        // socket.on('ENTER', function(msg){
+        //     socketList.forEach(function (item, i) {
+        //         if (item != socket) {
+        //             item.emit('ENTER', `${msg}`);
+        //         }
+        //     });
+        // });
 
         socket.on('disconnect', function () {
             socketList.splice(socketList.indexOf(socket), 1);
+            console.log(`유저 숫자(disconnect) : ${socketList.length}`);
+
             socketList.forEach(function (item, i) {
-                console.log(item.id);
                 if (item != socket) {
-                    item.emit('SEND', `${socket.request.session.authId}님이 나가셧습니다.`);
+                    //item.emit('EXIT', `${socket.request.session.authId}`);
+                    item.emit('SEND', `${socket.request.session.authId}이 나가셧습니다.`);
                 }
             });
         });
